37ebeaaf94277bcef217fb23b43095b6f8911d81
ananya30611 <24csu015@ncuindia.edu>
2024-12-01 02:59:25 +0530

    Add files via upload

---
+++ assign-2/q4.c
@@ -0,0 +1,24 @@
+//WAP to find Who & how many students have scored 99 in an array Marks.
+#include<stdio.h>
+int main (){
+   int num,x=0;
+    printf("Enter the number of students:");
+    scanf("%d",&num);
+    float marks[num];
+    for (int i=0;i<num;i++)
+    {
+        printf("Entered the marks of student %d:",i+1);
+        scanf("%f",&marks[i]);
+    }
+    printf("\n");
+    for (int i=0;i<num;i++)
+    {
+        if (marks[i]==99.0)
+        {
+            x+=1;
+            printf("Student %d scored 99 marks.\n",i+1);
+        }
+    }
+    printf("The number of students with 99 marks are %d.",x);
+    return 0;
+}
