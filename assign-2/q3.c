4774431b3247116e87384d3c9e30abc78f150b0c
ananya30611 <24csu015@ncuindia.edu>
2024-12-01 02:57:02 +0530

    Add files via upload

---
+++ assign-2/q3.c
@@ -0,0 +1,23 @@
+// WAP to find who scored first “99” in an array marks.
+#include<stdio.h>
+int main (){
+int num;
+    printf("Enter the number of students:");
+    scanf("%d",&num);
+    float (marks[num]);
+    for (int i=0;i<num;i++)
+    {
+        printf("Enter marks of the student %d:",i+1);
+        scanf("%f",&marks[i]);
+    }
+    printf("\n");
+    for (int i=0;i<num;i++)
+    {
+        if (marks[i]==99.0)
+        {
+            printf("Student %d scored the first 99 marks.",i+1);
+            break;
+        }
+    }
+    return 0;
+}
